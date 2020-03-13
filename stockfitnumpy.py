31a721ced2bac583c9477b4a3f435a4a15721f4e
win <ssszmath@gmail.com>
2020-03-13 10:04:16 +0330

    add etkam iran stock market dataset in project
    plot deg 6 polyinomial of etkam dataset

--- stockfitnumpy.py
+++ stockfitnumpy.py
@@ -9,13 +9,17 @@ def creatFurmula(z):
         t += '+' + str(z[i]) + '*x**' + str(len(z) - 1 - i)
     return t
 
-data = pd.read_csv('zbh2.csv')
-x1=data['Date value']
-y1=data['Close']
-x=np.array(x1)
-y=np.array(y1)
-z=np.polyfit(x,y ,7)
 
-plt.plot(x1,y1)
+data = pd.read_csv('etkam.csv')
+x1 = data['Date value']
+y1 = data['Close']
+x = np.array(x1)
+y = np.array(y1)
+z = np.polyfit(x, y, 6)
+p = np.array(range(len(x)+2,len(x)+22))
+x = np.append(x,p)
+
+
+plt.plot(x1, y1)
 plt.plot(x, eval(creatFurmula(z)))
 plt.show()
